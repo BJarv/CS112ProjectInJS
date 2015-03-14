6dc7d8f716e67df98c4d220f5dca438fb2dfbfd2
gcherr <gcherrer@ucsc.edu>
2015-03-14 13:49:46 -0700

    Revert "Revert "added restart implementation""
    This reverts commit 53f42556c49cd4dee73c2d4330bccd59dca9b670.

---
+++ Assets/Scripts/restart.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var restartAfter : float = 2f;
+
+function Awake () {
+	StartCoroutine("LoadMenu");
+}
+
+function LoadMenu() {
+	yield new WaitForSeconds(restartAfter);
+	Application.LoadLevel("Menu");
+}
